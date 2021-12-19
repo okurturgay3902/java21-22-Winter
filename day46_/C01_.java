e4968073952217f2de6a2e2fd06aa47e8d7a3006
okurturgay3902 <94409068+okurturgay3902@users.noreply.github.com>
2021-12-19 23:27:13 +0100

    1912es

---
+++ day46_/C01_.java
@@ -0,0 +1,23 @@
+package day46_;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class C01_ {
+
+	public static void main(String[] args) {
+		
+		List<String> list=new ArrayList<>();
+		
+		list.add("ali");
+		
+		System.out.println(list);
+		
+		list.clear();
+		
+		System.out.println(list);
+		
+
+	}
+
+}
