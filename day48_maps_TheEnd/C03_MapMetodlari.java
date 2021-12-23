0cf0b4c85b8678fa642cd0b98ac9f949ad173885
okurturgay3902 <94409068+okurturgay3902@users.noreply.github.com>
2021-12-23 23:07:06 +0100

    theEnd

--- day48_maps_TheEnd/C03_MapMetodlari.java
+++ day48_maps_TheEnd/C03_MapMetodlari.java
@@ -7,52 +7,46 @@ import java.util.Set;
 public class C03_MapMetodlari {
 
 	public static void main(String[] args) {
-		
-		Map<Integer,String> ornek=MapOlustur.myMap();
-		System.out.println(ornek);
-		
-		System.out.println(ornek.containsKey(110)); //false
-		System.out.println(ornek.containsValue("JAVA"));//false bizim tek basina JAVA diye bir elemanimiz yok
-		System.out.println(ornek.containsValue("Mustafa, Kan, JAVA"));//true
 
-		Set<Entry<Integer,String>> entrySet = ornek.entrySet(); // Her bir elemanin adi entry 101=Ali, Can, java
-		
-		int sayac=1;
-		
-		for (Entry<Integer,String> each: entrySet) {
-			
-			System.out.println(sayac+ ".entry : "+ each);
-			
+		Map<Integer, String> ornek = MapOlustur.myMap();
+		System.out.println(ornek);
+
+		System.out.println(ornek.containsKey(110)); // false
+		System.out.println(ornek.containsValue("JAVA"));// false bizim tek basina JAVA diye bir elemanimiz yok
+		System.out.println(ornek.containsValue("Mustafa, Kan, JAVA"));// true
+
+		Set<Entry<Integer, String>> entrySet = ornek.entrySet(); // Her bir elemanin adi entry 101=Ali, Can, java
+
+		int sayac = 1;
+
+		for (Entry<Integer, String> each : entrySet) {
+
+			System.out.println(sayac + ".entry : " + each);
+
 			sayac++;
-			
+
 		}
-		
-		System.out.println(ornek.getOrDefault(110, "girilen key map'da yok")); //Ali, Yan, C#
-		System.out.println(ornek.getOrDefault(103, "girilen key map'da yok")); //Ali, Yan, C#
-		
+
+		System.out.println(ornek.getOrDefault(110, "girilen key map'da yok")); // Ali, Yan, C#
+		System.out.println(ornek.getOrDefault(103, "girilen key map'da yok")); // Ali, Yan, C#
+
 		ornek.putIfAbsent(103, "Ali, Yan, C#"); // eklemeyecek cunku 103 var zaten
-		
+
 		ornek.putIfAbsent(110, "Fikri, Yan, Qa");// ekledi cunku 110 yok
-		
-		for (Entry<Integer,String> each: entrySet) {
-			
-			System.out.println(sayac+ ".entry : "+ each);
-			
+
+		sayac = 1;
+
+		for (Entry<Integer, String> each : entrySet) {
+
+			System.out.println(sayac + ".entry : " + each);
+
 			sayac++;
-			
+
 		}
-		
-		String maptenString=ornek.toString(); //Stringe donusturdu
+
+		String maptenString = ornek.toString(); // Stringe donusturdu
 		System.out.println(maptenString);
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
 	}
 
 }
--- day48_maps_TheEnd/MapOlustur.java
+++ day48_maps_TheEnd/MapOlustur.java
@@ -14,6 +14,9 @@ public class MapOlustur {
 		ornekMap.put(101, "Ali, Can, java");
 		ornekMap.put(102, "Veli, Yan, java");
 		ornekMap.put(103, "Ali, Yan, C#");
+		ornekMap.put(104, "Mustafa, Kan, JAVA");
+		ornekMap.put(105, "Ridvan, Tan, C#");
+		ornekMap.put(106, "Kutlu, San, JaVa");
 		
 		return ornekMap;
 	}
